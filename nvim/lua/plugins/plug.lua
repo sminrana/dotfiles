9c41f485e0c36555d74f362318ea614c943528f1
Smin <sminrana@gmail.com>
2025-04-06 06:55:39 +0600

    added lualine

--- nvim/lua/plugins/plug.lua
+++ nvim/lua/plugins/plug.lua
@@ -215,4 +215,37 @@ return {
       snippetDir = "/Users/smin/Desktop/snippets/",
     },
   },
+
+
+  {
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    enabled = true,
+    opts = {
+      options = {
+        icons_enabled = true,
+        theme = 'tokyonight',
+        component_separators = '|',
+        section_separators = '',
+      },
+      sections = {
+        lualine_a = { 'mode' },
+        lualine_b = { 'branch', 'diff', 'diagnostics' },
+        lualine_c = { 'filename' },
+        lualine_x = { 'encoding', 'fileformat', 'filetype' },
+        lualine_y = { 'progress' },
+        lualine_z = { 'location' }
+      },
+      inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = { 'filename' },
+        lualine_x = { 'location' },
+        lualine_y = {},
+        lualine_z = {}
+      },
+      tabline = {},
+      extensions = {}
+    }
+  },
 }
