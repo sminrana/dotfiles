0f8dd141babe740f66a7a55c12c0b1d43348af41
Smin <sminrana@gmail.com>
2025-04-17 18:47:36 +0600

    Remove auto save

--- nvim/lua/plugins/plug.lua
+++ nvim/lua/plugins/plug.lua
@@ -215,14 +215,4 @@ return {
       snippetDir = "/Users/smin/Desktop/snippets/",
     },
   },
-  {
-    "Pocco81/auto-save.nvim",
-    opts = {
-      execution_message = {
-        message = function() -- message to print on save
-          return ""
-        end,
-      },
-    },
-  }
 }
