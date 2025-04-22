b6a338dab3b69afdcb87a2dcaba1b2170c93f4f8
Smin <sminrana@gmail.com>
2025-04-22 10:54:52 +0600

    added new key binding

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -35,6 +35,39 @@ config.window_padding = {
 local act = wezterm.action
 config.keys = {
 	{ mods = "CMD", key = "s", action = act.SendKey({ mods = "CTRL", key = "s" }) },
+	{ key = "d", mods = "CMD|SHIFT", action = wezterm.action.SplitVertical },
+	{ key = "d", mods = "CMD", action = wezterm.action.SplitHorizontal },
+	{ key = "LeftArrow", mods = "CMD", action = wezterm.action.ActivatePaneDirection("Left") },
+	{ key = "RightArrow", mods = "CMD", action = wezterm.action.ActivatePaneDirection("Right") },
+	{ key = "UpArrow", mods = "CMD", action = wezterm.action.ActivatePaneDirection("Up") },
+	{ key = "DownArrow", mods = "CMD", action = wezterm.action.ActivatePaneDirection("Down") },
+	{ key = "w", mods = "CMD", action = wezterm.action.CloseCurrentTab({ confirm = true }) },
+	{
+		key = 'w',
+		mods = 'CMD',
+		action = wezterm.action.CloseCurrentPane { confirm = true },
+	},
+	{ key = "t", mods = "CMD|SHIFT", action = wezterm.action.SpawnTab("CurrentPaneDomain") },
+	{ key = "f", mods = "CMD|SHIFT", action = wezterm.action.ToggleFullScreen },
+	{key="p", mods="CMD|SHIFT",
+		action=wezterm.action{PaneSelect={alphabet="0123456789"}}
+    },
+	{
+		key = 'LeftArrow',
+		mods = 'CMD|SHIFT',
+		action = act.AdjustPaneSize { 'Left', 5 },
+	},
+	{
+		key = 'DownArrow',
+		mods = 'CMD|SHIFT',
+		action = act.AdjustPaneSize { 'Down', 5 },
+	},
+	{ key = 'UpArrow', mods = 'CMD|SHIFT', action = act.AdjustPaneSize { 'Up', 5 } },
+	{
+		key = 'RightArrow',
+		mods = 'CMD|SHIFT',
+		action = act.AdjustPaneSize { 'Right', 5 },
+	},
 }
 
 -- and finally, return the configuration to wezterm
