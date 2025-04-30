a14b285533ea7a372d1492aa11770a62771679e9
Smin <sminrana@gmail.com>
2025-05-01 00:53:35 +0600

    Added markdown preview

--- nvim/lua/plugins/plug.lua
+++ nvim/lua/plugins/plug.lua
@@ -258,4 +258,13 @@ return {
       vim.g.loaded_netrwPlugin = 1
     end,
   },
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    build = "cd app && yarn install",
+    init = function()
+      vim.g.mkdp_filetypes = { "markdown" }
+    end,
+    ft = { "markdown" },
+  },
 }
