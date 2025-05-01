aaeabfd4aaa2ad4a4f1a7d34768ed78ba4a184a3
Smin <sminrana@gmail.com>
2025-05-01 12:21:54 +0600

    Updated config

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -8,20 +8,21 @@ local config = wezterm.config_builder()
 
 -- For example, changing the color scheme:
 config.color_scheme = "Tokyo Night"
-
-config.font = wezterm.font("JetBrains Mono")
+config.bold_brightens_ansi_colors = "BrightOnly"
+config.font =
+	wezterm.font({ family = "JetBrains Mono", weight = "Medium", harfbuzz_features = { "calt=0", "clig=0", "liga=0" } })
 config.font_size = 16.0
 config.freetype_load_target = "Light"
 config.freetype_render_target = "HorizontalLcd"
-config.front_end = 'OpenGL'
+config.front_end = "OpenGL"
 config.cell_width = 0.9
-config.freetype_load_flags = 'FORCE_AUTOHINT'
-config.window_close_confirmation = 'AlwaysPrompt'
-config.default_cursor_style = 'BlinkingBar'
+config.freetype_load_flags = "NO_HINTING"
+config.window_close_confirmation = "AlwaysPrompt"
+config.default_cursor_style = "BlinkingBar"
 
 config.enable_tab_bar = true
 config.hide_tab_bar_if_only_one_tab = true
-config.window_background_opacity = 0.98
+config.window_background_opacity = 0.90
 config.window_decorations = "RESIZE"
 config.use_fancy_tab_bar = true
 
@@ -43,30 +44,28 @@ config.keys = {
 	{ key = "DownArrow", mods = "CMD", action = wezterm.action.ActivatePaneDirection("Down") },
 	{ key = "w", mods = "CMD", action = wezterm.action.CloseCurrentTab({ confirm = true }) },
 	{
-		key = 'w',
-		mods = 'CMD',
-		action = wezterm.action.CloseCurrentPane { confirm = true },
+		key = "w",
+		mods = "CMD",
+		action = wezterm.action.CloseCurrentPane({ confirm = true }),
 	},
 	{ key = "t", mods = "CMD|SHIFT", action = wezterm.action.SpawnTab("CurrentPaneDomain") },
 	{ key = "f", mods = "CMD|SHIFT", action = wezterm.action.ToggleFullScreen },
-	{key="p", mods="CMD|SHIFT",
-		action=wezterm.action{PaneSelect={alphabet="0123456789"}}
-    },
+	{ key = "p", mods = "CMD|SHIFT", action = wezterm.action({ PaneSelect = { alphabet = "0123456789" } }) },
 	{
-		key = 'LeftArrow',
-		mods = 'CMD|SHIFT',
-		action = act.AdjustPaneSize { 'Left', 5 },
+		key = "LeftArrow",
+		mods = "CMD|SHIFT",
+		action = act.AdjustPaneSize({ "Left", 5 }),
 	},
 	{
-		key = 'DownArrow',
-		mods = 'CMD|SHIFT',
-		action = act.AdjustPaneSize { 'Down', 5 },
+		key = "DownArrow",
+		mods = "CMD|SHIFT",
+		action = act.AdjustPaneSize({ "Down", 5 }),
 	},
-	{ key = 'UpArrow', mods = 'CMD|SHIFT', action = act.AdjustPaneSize { 'Up', 5 } },
+	{ key = "UpArrow", mods = "CMD|SHIFT", action = act.AdjustPaneSize({ "Up", 5 }) },
 	{
-		key = 'RightArrow',
-		mods = 'CMD|SHIFT',
-		action = act.AdjustPaneSize { 'Right', 5 },
+		key = "RightArrow",
+		mods = "CMD|SHIFT",
+		action = act.AdjustPaneSize({ "Right", 5 }),
 	},
 }
 
