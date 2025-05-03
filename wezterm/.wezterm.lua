1a2c6ec1d2cc9b6836bba9e4a86d9de199e1deb3
Smin <sminrana@gmail.com>
2025-05-03 16:34:11 +0600

    updated wez

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -22,7 +22,7 @@ config.default_cursor_style = "BlinkingBar"
 
 config.enable_tab_bar = true
 config.hide_tab_bar_if_only_one_tab = true
-config.window_background_opacity = 0.90
+config.window_background_opacity = 0.95
 config.window_decorations = "RESIZE"
 config.use_fancy_tab_bar = true
 
