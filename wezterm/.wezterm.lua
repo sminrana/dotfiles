2364e808237cba7e8b9e23f103c22e881680695c
Smin <sminrana@gmail.com>
2025-05-08 12:47:47 +0600

    wezterm updated

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -9,7 +9,7 @@ local config = wezterm.config_builder()
 config.color_scheme = "Tokyo Night"
 config.bold_brightens_ansi_colors = "BrightOnly"
 config.font =
-	wezterm.font({ family = "JetBrains Mono", weight = "Medium", harfbuzz_features = { "calt=0", "clig=0", "liga=0" } })
+	wezterm.font({ family = "JetBrainsMono Nerd Font", weight = "Medium"})
 config.font_size = 16.0
 config.freetype_load_target = "Light"
 config.freetype_render_target = "HorizontalLcd"
