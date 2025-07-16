85fa1cfb5c8bbfbfb949d9502f7d1c08d93e8b0e
Smin <sminrana@gmail.com>
2025-07-16 23:05:32 +0600

    Updated null-ls

--- nvim/lua/plugins/null-ls.lua
+++ nvim/lua/plugins/null-ls.lua
@@ -1,11 +1,25 @@
 return {
-    "nvimtools/none-ls.nvim",
-    opts = function(_, opts)
-        local null_ls = require("null-ls")
-        table.insert(opts.sources, null_ls.builtins.formatting.prettier.with({
-            filetypes = { "blade" },
-            command = "npx",
-            args = { "prettier", "--stdin-filepath", "$FILENAME" },
-        }))
-    end,
+  "nvimtools/none-ls.nvim",
+  opts = function(_, opts)
+    local null_ls = require("null-ls")
+    opts.sources = vim.list_extend(opts.sources or {}, {
+      -- Diagnostics from phpstan
+      null_ls.builtins.diagnostics.phpstan.with({
+        command = "phpstan", -- Make sure it's in $PATH
+        extra_args = { "--level=max" },
+      }),
+
+      -- Formatting with phpcsfixer
+      null_ls.builtins.formatting.phpcsfixer.with({
+        command = "php-cs-fixer",
+        extra_args = { "fix", "--using-cache=no" },
+      }),
+
+      null_ls.builtins.formatting.prettier.with({
+        filetypes = { "blade" },
+        command = "npx",
+        args = { "prettier", "--stdin-filepath", "$FILENAME" },
+      }),
+    })
+  end,
 }
