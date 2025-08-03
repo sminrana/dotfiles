433ab00bf39439d7fba65e16ced7d28b1a0953c7
Smin <sminrana@gmail.com>
2025-08-03 23:34:21 +0600

    updated null-ls

--- nvim/lua/plugins/null-ls.lua
+++ nvim/lua/plugins/null-ls.lua
@@ -3,17 +3,17 @@ return {
   opts = function(_, opts)
     local null_ls = require("null-ls")
     opts.sources = vim.list_extend(opts.sources or {}, {
-      -- Diagnostics from phpstan
-      -- null_ls.builtins.diagnostics.phpstan.with({
-      --   command = "phpstan", -- Make sure it's in $PATH
-      --   extra_args = { "--level=max" },
-      -- }),
-
       -- Formatting with phpcsfixer
       -- null_ls.builtins.formatting.phpcsfixer.with({
       --   command = "php-cs-fixer",
       --   extra_args = { "fix", "--using-cache=no" },
       -- }),
+      --
+      null_ls.builtins.diagnostics.phpstan.with({
+        command = "phpstan",
+        args = { "analyze", "--error-format", "raw", "$FILENAME" },
+        method = null_ls.methods.DIAGNOSTICS_ON_SAVE,
+      }),
 
       null_ls.builtins.formatting.prettier.with({
         filetypes = { "blade" },
