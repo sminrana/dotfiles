219e4dbd39ec000fa4c5e4cc054dffc244f3d9e4
Smin <sminrana@gmail.com>
2025-09-18 01:14:47 +0600

    Updated filediff plugin

--- nvim/lua/plugins/filediff.lua
+++ nvim/lua/plugins/filediff.lua
@@ -1,9 +1,24 @@
 return {
   "sminrana/nvim-filediff",
+  event = "VeryLazy", -- ensures keymaps load
   config = function()
     local filediff = require("filediff")
-    vim.keymap.set("n", "<leader>jdf", filediff.FileDiff, { desc = "Diff two files" })
-    vim.keymap.set("n", "<leader>jdi", filediff.FileDiffInputs, { desc = "Diff via input paths" })
-    vim.keymap.set("n", "<leader>jdd", filediff.FolderDiff, { desc = "Diff two folders (require absolute path)" })
+
+    vim.keymap.set("n", "<leader>jdf", "<cmd>FileDiff<CR>", { desc = "Diff two files" })
+    vim.keymap.set("n", "<leader>jdi", "<cmd>FileDiffInput<CR>", { desc = "Diff via input paths" })
+    vim.keymap.set("n", "<leader>jdd", "<cmd>FolderDiff<CR>", { desc = "Diff two folders" })
   end,
 }
+
+-- return {
+--   dir = "~/github/nvim-filediff", -- exact path to your plugin directory
+--   name = "filediff",
+--   lazy = false, -- Changed to false so it loads immediately
+--   config = function()
+--     local filediff = require("filediff")
+
+--       vim.keymap.set("n", "<leader>jdf", "<cmd>FileDiff<CR>", { desc = "Diff two files" })
+--       vim.keymap.set("n", "<leader>jdi", "<cmd>FileDiffInput<CR>", { desc = "Diff via input paths" })
+--       vim.keymap.set("n", "<leader>jdd", "<cmd>FolderDiff<CR>", { desc = "Diff two folders" })
+--   end,
+-- }
