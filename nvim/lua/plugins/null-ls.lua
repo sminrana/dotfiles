ebf6356199cf150a71158bb1bc8648a2bc27d8b2
Nafiz <sminrana@gmail.com>
2025-10-08 01:51:57 +0600

    Added xcode

--- nvim/lua/plugins/null-ls.lua
+++ nvim/lua/plugins/null-ls.lua
@@ -4,10 +4,10 @@ return {
     local null_ls = require("null-ls")
     opts.sources = vim.list_extend(opts.sources or {}, {
       -- Formatting with phpcsfixer
-      -- null_ls.builtins.formatting.phpcsfixer.with({
-      --   command = "php-cs-fixer",
-      --   extra_args = { "fix", "--using-cache=no" },
-      -- }),
+      null_ls.builtins.formatting.phpcsfixer.with({
+        command = "php-cs-fixer",
+        extra_args = { "fix", "--using-cache=no" },
+      }),
       --
       null_ls.builtins.diagnostics.phpstan.with({
         command = "phpstan",
