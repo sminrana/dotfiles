b039b048882cd01d9bbb4f9252972180529f64c9
Nafiz <sminrana@gmail.com>
2026-01-16 09:17:47 +0600

    Remove redundant keymap

--- nvim/lua/plugins/diffview.lua
+++ nvim/lua/plugins/diffview.lua
@@ -9,7 +9,6 @@ return {
     vim.keymap.set("n", "<leader>gdo", ":DiffviewOpen<CR>", { desc = "Diffview: open" })
     vim.keymap.set("n", "<leader>gdc", ":DiffviewClose<CR>", { desc = "Diffview: close" })
     vim.keymap.set("n", "<leader>gdh", ":DiffviewFileHistory<CR>", { desc = "Diffview: file history" })
-    vim.keymap.set("n", "<leader>gdl", ":DiffviewFileHistory<CR>", { desc = "Diffview: Log" })
 
     -- Open current file history in Diffview
     vim.keymap.set("n", "<leader>gdH", function()
