a4473558e81a102c6d95f5c957a2340424ea85e5
Nafiz <sminrana@gmail.com>
2026-02-22 23:46:12 +0600

    Updated diffview

--- nvim/lua/plugins/diffview.lua
+++ nvim/lua/plugins/diffview.lua
@@ -1,42 +1,50 @@
 return {
   "sindrets/diffview.nvim",
-  event = "VeryLazy",
-  config = function()
-    local diffview = require("diffview")
+    event = "VeryLazy",
+    config = function()
+      local diffview = require("diffview")
 
-    local actions = require("diffview.actions")
-    diffview.setup({
-      enhanced_diff_hl = true,
+      local actions = require("diffview.actions")
+      diffview.setup({
+        enhanced_diff_hl = true,
 
-      view = {
-        default = {
-          layout = "diff2_horizontal", -- side-by-side like GitHub
-          winbar_info = true,
-        },
-        merge_tool = {
-          layout = "diff3_mixed",
-        },
-      },
-
-      file_panel = {
-        listing_style = "tree", -- fast mental map
-        win_config = {
-          width = 10,
-        },
-      },
-
-      keymaps = {
         view = {
-          ["q"] = actions.close,
+          default = {
+            layout = "diff2_horizontal", -- side-by-side like GitHub
+            winbar_info = true,
+          },
+          merge_tool = {
+            layout = "diff3_mixed",
+          },
         },
-      },
-    })
 
-    -- 🔑 REVIEW HOTKEYS (muscle-memory friendly)
-    vim.keymap.set("n", "<leader>gd", "<Cmd>DiffviewOpen<CR>", { desc = "Review: open Diffview" })
-    vim.keymap.set("n", "<leader>gD", "<Cmd>DiffviewClose<CR>", { desc = "Review: close Diffview" })
-    vim.keymap.set("n", "<leader>gF", "<Cmd>DiffviewFileHistory<CR>", { desc = "Review: files history" })
-    vim.keymap.set("n", "<leader>gH", "<Cmd>DiffviewFileHistory %<CR>", { desc = "Review: current file history" })
-    vim.keymap.set("n", "<leader>gm", "<Cmd>DiffviewOpen origin/main<CR>", { desc = "Review: vs origin/main" })
-  end,
-}
+        file_panel = {
+          listing_style = "tree", -- fast mental map
+          win_config = {
+            width = 10,
+          },
+        },
+
+        keymaps = {
+          view = {
+            ["q"] = actions.close,
+          },
+        },
+
+        hooks = {
+          diff_buf_read = function(bufnr)
+            vim.api.nvim_buf_call(bufnr, function()
+              vim.cmd("normal! zR")
+            end)
+          end,
+        },
+      })
+
+      -- 🔑 REVIEW HOTKEYS (muscle-memory friendly)
+      vim.keymap.set("n", "<leader>gd", "<Cmd>DiffviewOpen<CR>", { desc = "Review: open Diffview" })
+      vim.keymap.set("n", "<leader>gD", "<Cmd>DiffviewClose<CR>", { desc = "Review: close Diffview" })
+      vim.keymap.set("n", "<leader>gF", "<Cmd>DiffviewFileHistory<CR>", { desc = "Review: files history" })
+      vim.keymap.set("n", "<leader>gH", "<Cmd>DiffviewFileHistory %<CR>", { desc = "Review: current file history" })
+      vim.keymap.set("n", "<leader>gm", "<Cmd>DiffviewOpen origin/main<CR>", { desc = "Review: vs origin/main" })
+    end,
+  }
