f18f65c670584a33325650ea7582afa480f503af
Nafiz <sminrana@gmail.com>
2026-04-06 09:34:56 +0600

    chore(nvim): remove lazy-lock and unused plugins
    Removed the `lazy-lock.json` file and added it to `.gitignore` to
    prevent
    tracking plugin lock files. Also removed the
    `lazyvim.plugins.extras.util.project`
    plugin from `lazyvim.json` and the `blink.cmp` dependency from the
    `codecompanion.lua` plugin configuration. This cleans up unused or
    unnecessary plugin references and files.

--- nvim/lua/plugins/codecompanion.lua
+++ nvim/lua/plugins/codecompanion.lua
@@ -3,23 +3,6 @@ return {
   dependencies = {
     { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
     { "nvim-lua/plenary.nvim" },
-    -- Test with blink.cmp
-    {
-      "saghen/blink.cmp",
-      lazy = false,
-      version = "*",
-      opts = {
-        keymap = {
-          preset = "enter",
-          ["<S-Tab>"] = { "select_prev", "fallback" },
-          ["<Tab>"] = { "select_next", "fallback" },
-        },
-        cmdline = { sources = { "cmdline" } },
-        sources = {
-          default = { "lsp", "path", "buffer", "codecompanion" },
-        },
-      },
-    },
   },
   opts = {
     adapters = {
@@ -37,8 +20,8 @@ return {
   keys = {
     { "<leader>axa", "<cmd>CodeCompanionActions<cr>", desc = "CodeCompanion Actions" },
     { "<leader>axc", "<cmd>CodeCompanionChat Toggle<cr>", desc = "CodeCompanion Chat Toggle" },
-    { "<leader>axi", "<cmd>CodeCompanion<cr>", mode = "n", desc = "CodeCompanion Inline" },
-    { "<leader>axi", ":'<,'>CodeCompanion<cr>", mode = "v", desc = "CodeCompanion Inline (Selection)" },
+    { "<leader>av", "<cmd>CodeCompanion<cr>", mode = "n", desc = "CodeCompanion Inline" },
+    { "<leader>av", ":'<,'>CodeCompanion<cr>", mode = "v", desc = "CodeCompanion Inline (Selection)" },
   },
 }
 
